255cde7f59bd710f1d46ea77ec931eef589b72b0
KJam <kaytchjam@gmail.com>
2022-12-19 09:56:30 -0600

    green triangle made

--- OpenGLIntro/src/MyApp.cpp
+++ OpenGLIntro/src/MyApp.cpp
@@ -2,6 +2,49 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int compileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str(); // returns a pointer to our source std::string
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)_malloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(type);
+        return 0;
+    }
+
+    // TODO: Error Handling
+
+    return id;
+}
+
+static unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    // take care of the OBJ files
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
+
 int main(void) {
     GLFWwindow* window;
 
@@ -12,8 +55,7 @@ int main(void) {
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
@@ -26,25 +68,50 @@ int main(void) {
     }
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+    
 
+    // TRIANGLE GENERATION
     float positions[6] = {
-        -0.5f, -0.5f, // vertex 1
-         0.0f,  0.0f, // vertex 2
-         0.5f, -0.5f, // vertex 3
+        -0.5f, -0.5f, // position vertex 1 (x, y)
+         0.0f,  0.5f, // position vertex 2 (x, y)
+         0.5f, -0.5f, // position vertex 3 (x, y)
     };
-
+    
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    std::string vertexShader = R"glsl(#version 330 core
+
+layout(location = 0) in vec4 position;
+
+void main(){
+    gl_Position = position;
+}
+)glsl";
+
+    std::string fragmentShader = R"glsl(#version 330 core
+
+layout(location = 0) out vec4 color;
+
+void main(){
+    color = vec4(0.5, 0.5, 0.0, 1.0);
+}
+)glsl";
+
+    unsigned int shader = createShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
         // Rendering a triangle using legacy openGL (BEGIN)
@@ -63,6 +130,7 @@ int main(void) {
         glfwPollEvents();
     }
 
+    glDeleteProgram(shader);
     glfwTerminate();
     return 0;
 }
