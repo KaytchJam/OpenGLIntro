e5f40388ed3db86243bf5634e31ac1dc821eced8
KJam <kaytchjam@gmail.com>
2022-12-20 11:14:53 -0600

    IndexBuffer and VertexBuffer abstracted away into their own classes

--- OpenGLIntro/src/MyApp.cpp
+++ OpenGLIntro/src/MyApp.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 /// <summary>
 /// Struct with the source code of our shader types as string fields
@@ -64,8 +67,6 @@ static unsigned int compileShader(unsigned int type, const std::string& source) 
         return 0;
     }
 
-    // TODO: Error Handling
-
     return id;
 }
 
@@ -93,6 +94,10 @@ int main(void) {
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -104,55 +109,90 @@ int main(void) {
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK) {
         std::cout << "Error!" << std::endl;
     }
 
     std::cout << glGetString(GL_VERSION) << std::endl;
-    
+
 
     // SQUARE GENERATION
+    //float positions[12] = {
+    //    -0.5f, -0.5f, // position vertex 1 (x, y) - index 0
+    //     0.5f, -0.5f, // position vertex 2 (x, y) - index 1
+    //     0.5f,  0.5f, // position vertex 3 (x, y) - index 2
+    //    -0.5f, 0.5f   // position vertex 4 (x, y) - index 3
+    //};
+
+    //// our 'indexBuffer' indicating the order in which to draw our shape
+    //unsigned int indices[] = {
+    //    0, 1, 2, // triangle 1
+    //    2, 3, 0 // triangle 2
+    //};         // together they form a square
+
+
+    // DRAWING A TRAPEZOID
     float positions[12] = {
-        -0.5f, -0.5f, // position vertex 1 (x, y) - index 0
-         0.5f, -0.5f, // position vertex 2 (x, y) - index 1
-         0.5f,  0.5f, // position vertex 3 (x, y) - index 2
-        -0.5f, 0.5f   // position vertex 4 (x, y) - index 3
+        -0.5, 0.5,
+        -0.7, -0.5,
+         0.7, -0.5,
+         0.5, 0.5
     };
 
-    // our 'indexBuffer' indicating the order in which to draw our shape
     unsigned int indices[] = {
-        0, 1, 2, // triangle 1
-        2, 3, 0 // triangle 2
-    };         // together they form a square
+        0, 1, 2,
+        2, 3, 0
+    };
 
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
 
-    
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
-    
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
-    unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
+    IndexBuffer ib(indices, 6);
    
     ShaderProgramSource shaderSource = parseShader("res/shaders/Basic.shader");
     unsigned int shader = createShader(shaderSource.vertexSource, shaderSource.fragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //glDrawArrays(GL_TRIANGLES, 0, 6); // without indexBuffer
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); // with indexBuffer
+        GLCall(glUseProgram(shader));
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
+        GLCall(glBindVertexArray(vao));
+        ib.Bind();
+
+        //glDrawArrays(GL_TRIANGLES, 0, 6); // without indexBuffer
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr)); // with indexBuffer
+
+        if (r > 1.0f) {
+            increment = -0.05f;
+        } else if (r < 0.0f) {
+            increment = 0.05f;
+        }
+
+        r += increment;
 
         // Rendering a triangle using legacy openGL (BEGIN)
         /*glBegin(GL_TRIANGLES);
