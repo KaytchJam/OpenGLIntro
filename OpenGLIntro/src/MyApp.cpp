a6e60c7a8b83d666a6e60d9fefeb93fac7f6010d
KJam <kaytchjam@gmail.com>
2022-12-23 14:25:16 -0600

    interactive imGui interface

--- OpenGLIntro/src/MyApp.cpp
+++ OpenGLIntro/src/MyApp.cpp
@@ -114,6 +114,8 @@ int main(void) {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
     // IMGUI END
+    
+    glm::vec3 translation(200, 200, 0);
 
     bool show_demo_window = true;
     bool show_another_window = false;
@@ -133,8 +135,17 @@ int main(void) {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        // here
+        //glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        //glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0), translation);
+        glm::mat4 mvp = proj * view * model;
+
+        // end here
+
         shader.Bind();
         shader.setUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+        shader.setUniformMat4f("u_ModelViewProjection", mvp);
 
         va.Bind();
         ib.Bind();
@@ -154,18 +165,10 @@ int main(void) {
             static float f = 0.0f;
             static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Begin("Debugging Window");                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -175,7 +178,6 @@ int main(void) {
         //ImGui::EndFrame();
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
         // IMGUI END
 
         /* Swap front and back buffers */
