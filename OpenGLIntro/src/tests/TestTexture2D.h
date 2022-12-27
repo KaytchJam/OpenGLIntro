45a901e01b008ef1bd5d74731af2964fa9cdda03
KJam <kaytchjam@gmail.com>
2022-12-27 17:20:09 -0600

    filled out testtexture2d class functions in header

--- OpenGLIntro/src/tests/TestTexture2D.h
+++ OpenGLIntro/src/tests/TestTexture2D.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "Test.h"
+
+namespace test {
+	class TestTexture2D : public Test {
+	public:
+		TestTexture2D();
+		~TestTexture2D();
+
+		void onUpdate(float deltaTime);
+		void onRender();
+		void onImGuiRender();
+	private:
+		float m_ClearColor[4]; // RBGA
+	};
+}
