8f1462db264d48f878303a4798bd68ad5b93293b
KJam <kaytchjam@gmail.com>
2022-12-28 17:39:45 -0600

    outline code made

--- SegDisplaySim/src/main.cpp
+++ SegDisplaySim/src/main.cpp
@@ -9,5 +9,17 @@ int main(void) {
 	// have buttons on the side
 		// choose the "number to display"
 	// display that number in the form of a seven segment digit
+
+	segdisp::SegmentedDisplay seg;
+
+	uint8_t digit = 0;
+	bool on = true;
+	int mapping = seg.getMapping(digit, on);
+	std::cout << seg.mappingToString(mapping);
+		
+
+	// renderScreen
+	// renderGUI
+	// segmentLogic
 	return 0;
 }
