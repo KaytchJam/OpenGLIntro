7ed8e0d6f0dd6e071e62ee34425507736e2c7e88
KJam <kaytchjam@gmail.com>
2022-12-28 08:17:23 -0600

    Clearing out comments in MyApp.cpp

--- OpenGLIntro/src/MyApp.cpp
+++ OpenGLIntro/src/MyApp.cpp
@@ -52,51 +52,8 @@ int main(void) {
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    // DRAWING A SQUARE
-    //float positions[] = {
-    //    -50.0f, -50.0f,  0.0f, 0.0f, // bottom left of square - 0
-    //    50.0f, -50.0f,  1.0f, 0.0f, // bottom right - 1
-    //    50.0f, 50.0f,  1.0f, 1.0f, // top right - 2
-    //    -50.0f, 50.0f, 0.0f,  1.0f  // top left - 3
-    //};
-
-    //unsigned int indices[] = {
-    //    0, 1, 2,
-    //    2, 3, 0
-    //};
-
-
     glEnable(GL_BLEND);
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-    //VertexArray va;
-    //VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-    //VertexBufferLayout layout;
-    //layout.push<float>(2);
-    //layout.push<float>(2);
-    //va.addBuffer(vb, layout);
-
-    //IndexBuffer ib(indices, 6);
-
-    //// generate orthogonal matrix for projection
-    ////                          X DOMAIN      Y DOMAIN      Z DOMAIN
-    //// CONVERTS OUR POSITION COORDINATES TO FIT WITHIN THIS DOMAIN
-    //glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    //glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-    //Shader shader("res/shaders/Basic.shader");
-    //shader.Bind();
-    //shader.setUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-
-    //Texture texture("res/textures/Data_Heart.png");
-    //texture.Bind();
-    //shader.setUniform1i("u_Texture", 0);
-
-    //va.UnBind();
-    //shader.UnBind();
-    //vb.UnBind();
-    //ib.UnBind();
 
     Renderer renderer;
 
@@ -166,53 +123,6 @@ int main(void) {
         ImGui::EndFrame();
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        //// Object 1 Rendering
-        //{
-        //    glm::mat4 model = glm::translate(glm::mat4(1.0), translationA);
-        //    glm::mat4 mvp = proj * view * model;
-        //    shader.Bind();
-        //    shader.setUniformMat4f("u_ModelViewProjection", mvp);
-        //    renderer.Draw(va, ib, shader);
-        //}
-
-        //// Object 2 Rendering
-        //{
-        //    glm::mat4 model = glm::translate(glm::mat4(1.0), translationB);
-        //    glm::mat4 mvp = proj * view * model;
-        //    shader.Bind();
-        //    shader.setUniformMat4f("u_ModelViewProjection", mvp);
-        //    renderer.Draw(va, ib, shader);
-        //}
-
-        //if (r > 1.0f) {
-        //    increment = -0.05f;
-        //} else if (r < 0.0f) {
-        //    increment = 0.05f;
-        //}
-
-        //r += increment;
-
-        //// IMGUI STUFF
-        //{
-        //    static float f = 0.0f;
-        //    static int counter = 0;
-
-        //    ImGui::Begin("Debugging Window");                          // Create a window called "Hello, world!" and append into it.
-
-        //    ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-        //    ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-        //    ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-        //    ImGui::Text("counter = %d", counter);
-
-        //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        //    ImGui::End();
-        //}
-
-        ////ImGui::EndFrame();
-        //ImGui::Render();
-        //ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        //// IMGUI END
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
