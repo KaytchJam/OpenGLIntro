7789d1d49d38b74fe6926b816f86c87406bcb703
KJam <kaytchjam@gmail.com>
2022-12-27 19:24:40 -0600

    made TestTexture2D contructor

--- OpenGLIntro/src/tests/TestTexture2D.cpp
+++ OpenGLIntro/src/tests/TestTexture2D.cpp
@@ -2,7 +2,11 @@
 
 namespace test {
 
-	TestTexture2D::TestTexture2D() {
+	TestTexture2D::TestTexture2D() : 
+		proj(glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f)),
+		view(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0))),
+		translationA(200, 200, 0),
+		translationB(400, 200, 0) {
 
 		float positions[] = {
 			-50.0f, -50.0f, 0.0f, 0.0f,
@@ -19,9 +23,34 @@ namespace test {
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+		VertexBufferLayout layout;
+		layout.push<float>(2);
+		layout.push<float>(2);
+		m_VA->addBuffer(vb, layout);
+
+		m_IB = new IndexBuffer(indices, 6);
+
+		Shader shader("res/shaders/Basic.shader");
+		m_Sh = new Shader("res/shaders/Basic.shader");
+		m_Sh->Bind();
+
+		m_Tx = new Texture("res/textures.Data_Heart.png");
+		m_Tx->Bind();
+		m_Sh->setUniform4f("u_Texture", 0);
+
+		m_VA->UnBind();
+		m_Sh->UnBind();
+		vb.UnBind();
+		m_IB->UnBind();
+		m_Tx->UnBind();
 	}
 
 	TestTexture2D::~TestTexture2D() {
+		delete m_VA; // delete vertex array
+		delete m_IB; // delete index buffer
+		delete m_Sh; // delete shader
+		delete m_Tx; // delete texture
 
 	}
 
--- OpenGLIntro/src/tests/TestTexture2D.h
+++ OpenGLIntro/src/tests/TestTexture2D.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "Test.h"
+#include "../VertexArray.h"
+#include "../IndexBuffer.h"
+#include "../Shader.h"
+#include "../Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 namespace test {
 	class TestTexture2D : public Test {
@@ -11,5 +18,16 @@ namespace test {
 		void onRender();
 		void onImGuiRender();
 	private:
+		// HEAP OBJECTS
+		VertexArray* m_VA = nullptr;
+		IndexBuffer* m_IB = nullptr;
+		Shader* m_Sh = nullptr;
+		Texture* m_Tx = nullptr;
+
+		// STACK OBJECTS
+		glm::mat4 proj;
+		glm::mat4 view;
+		glm::vec3 translationA;
+		glm::vec3 translationB;
 	};
 }
