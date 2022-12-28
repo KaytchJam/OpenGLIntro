41ae76e1a0c08a28936d164556492930646ba034
KJam <kaytchjam@gmail.com>
2022-12-28 17:16:33 -0600

    making outline of project rn

--- SegDisplaySim/src/main.cpp
+++ SegDisplaySim/src/main.cpp
@@ -3,5 +3,11 @@
 
 int main(void) {
 	std::cout << "Hello world" << std::endl;
+
+	// structure:
+	// create a window
+	// have buttons on the side
+		// choose the "number to display"
+	// display that number in the form of a seven segment digit
 	return 0;
 }
