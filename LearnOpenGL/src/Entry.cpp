0e31c6e3034a504052f692d9861321f2a09de5f9
KJam <kaytchjam@gmail.com>
2022-12-31 13:36:59 -0600

    Error occuring with GLAD initialization

--- LearnOpenGL/src/Entry.cpp
+++ LearnOpenGL/src/Entry.cpp
@@ -3,6 +3,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 int main() 
 {
 
@@ -13,9 +18,13 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// create window object
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "LearnOpenGL", glfwGetPrimaryMonitor(), NULL);
+	const int PROJECT_LENGTH = 1280;
+	const int PROJECT_HEIGHT = 720;
 
+	// create window object
+	GLFWwindow* window = glfwCreateWindow(PROJECT_LENGTH, PROJECT_HEIGHT, "LearnOpenGL", NULL, NULL);
+
+	// check for proper window creation
 	if (window == NULL) 
 	{
 		std::cout << "Window creation failed." << std::endl;
@@ -23,11 +32,23 @@ int main()
 		return -1;
 	}
 
+	// check for proper GLAD initialization
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
 	}
 
-	delete window;
+	glViewport(0, 0, PROJECT_LENGTH, PROJECT_HEIGHT); // indicate rendering window size
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// the render loop
+	while (!glfwWindowShouldClose(window)) // checks if the window has been 'told' to close
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents(); // checks if an event has been triggered (i.e. keyboard input)
+	}
+
+	glfwTerminate(); // clean all of glfw's allocated resources
 	return 0;
 }
