970c8270bbe12d6254e16ec212f0c1bd8087eca5
KJam <kaytchjam@gmail.com>
2023-01-03 20:27:01 -0600

    reorganized code, made sure to unbind the vertex array object prior to unbinding the vertex buffer object and element buffer object such that the vertex array retains those states when bound again

--- LearnOpenGL/src/Entry.cpp
+++ LearnOpenGL/src/Entry.cpp
@@ -151,10 +151,10 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
+	
 	// the above settings are associated with our currently bound vertex buffer object
 
 	// the render loop
@@ -168,7 +168,6 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vertexArrayObj);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObj);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
