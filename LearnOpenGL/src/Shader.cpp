d080de5b54d45ec8fc11e1677c725ded38bb8d68
KJam <kaytchjam@gmail.com>
2023-01-06 15:39:26 -0600

    implementing a shader parser

--- LearnOpenGL/src/Shader.cpp
+++ LearnOpenGL/src/Shader.cpp
@@ -1,6 +1,17 @@
 #include "Shader.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
 
 struct shaderStorage {
 	const std::string vertexShader;
 	const std::string fragmentShader;
 };
+
+static shaderStorage parseShaders(const std::string& vertexShaderPath, const std::string& fragmentShaderPath)
+{
+
+}
+
+
