6279b7b1a23745313fdbe7797824a53b8113be59
KJam <kaytchjam@gmail.com>
2023-01-09 17:50:01 -0600

    functioning linked list insertion policy (sorted order)

--- DrawLinkedList/src/MyLinkedList.h
+++ DrawLinkedList/src/MyLinkedList.h
@@ -32,16 +32,51 @@ myLinkedList * initializeLinkedList() {
 	myLinkedList* newLL = (myLinkedList*)malloc(sizeof(myLinkedList));
 	assert(newLL != NULL);
 
-	newLL->head = createNode(NULL, 0, NULL);
+	// header node that is never changed, using minimum 32 bit signed integer
+	newLL->head = createNode(NULL, 1 << ((sizeof(int) * 8) - 1), NULL);
+	newLL->head->next = newLL->head;
+	newLL->head->prev = newLL->head;
+	newLL->size = 0;
+
 	return newLL;
+}
+
+void printNode(llNode *newNode) {
+	printf("_______________\n");
+	printf("newNode data: %d\n", newNode->data);
+	printf("newNode prev data: %d\n", newNode->prev->data);
+	printf("newNode next data: %d\n", newNode->next->data);
+	printf("_______________\n");
 }
 
 void addFirst(myLinkedList *ll, int item) {
 	llNode* newNode = createNode(ll->head, item, ll->head->next);
-
+	ll->head->next = newNode;
+	ll->size++;
 }
 
-void addFirst(myLinkedList* ll) {
+void add(myLinkedList* ll, int item) {
+	llNode* temp = ll->head;
+	int count = -1;
+
+	while (count < ll->size - 1 && temp->next->data <= item) {
+		temp = temp->next;
+		count++;
+	}
+
+	llNode* newNode = createNode(temp, item, temp->next);
+	newNode->next->prev = newNode;
+	newNode->prev->next = newNode;
+	ll->size++;
 }
 
+void printList(myLinkedList* ll) {
+
+	llNode* temp = ll->head->next;
+	for (int i = 0; i < ll->size; i++) {
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
 
--- DrawLinkedList/src/main.cpp
+++ DrawLinkedList/src/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+	myLinkedList* ll = initializeLinkedList();
+	add(ll, 0);
+	add(ll, 1);
+	add(ll, 2);
+	add(ll, 3);
+
+	printf("Printall.\n");
+	printList(ll);
 	return 0;
 }
