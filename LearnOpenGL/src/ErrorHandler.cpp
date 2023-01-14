d590897de22e57715d18978750d48ba77eb3cb8d
KJam <kaytchjam@gmail.com>
2023-01-14 00:08:55 -0600

    seemingly fixed GLCall #define in ErrorHandler

---
+++ LearnOpenGL/src/ErrorHandler.cpp
@@ -0,0 +1,13 @@
+#include "ErrorHandler.h"
+
+void glClearError() {
+    while (glGetError());
+}
+
+bool glLogCall(const char* function, const char* file, int line) {
+    while (GLenum error = glGetError()) {
+        std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << " " << line << std::endl;
+        return false;
+    }
+    return true;
+}
