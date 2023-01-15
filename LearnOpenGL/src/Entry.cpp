9b7b13acd47afa26e06ee6a2bd1f1f44a9863883
KJam <kaytchjam@gmail.com>
2023-01-15 00:48:41 -0600

    fixing formatting

--- LearnOpenGL/src/Entry.cpp
+++ LearnOpenGL/src/Entry.cpp
@@ -508,13 +508,10 @@ extendedObjectIds textureSquare(std::string img_path, std::string img_path_2)
 	// flip loaded image along axis
 	stbi_set_flip_vertically_on_load(true);
 	unsigned char* data = stbi_load(img_path.c_str(), &width, &height, &nrChannels, 0);
-	if (data)
-	{
+	if (data) {
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
+	} else {
 		std::cout << "Failed to load texture" << std::endl;
 	}
 	stbi_image_free(data);
